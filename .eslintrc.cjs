7f5371899628b7aecd4a5b8949452a84dbb82a0e
seiha-tsgue <seiha.tsuge@gmail.com>
2022-10-13 21:02:25 +0900

    feat: ESLint, Prettierの設定を追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next', 'next/core-web-vitals', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+};
